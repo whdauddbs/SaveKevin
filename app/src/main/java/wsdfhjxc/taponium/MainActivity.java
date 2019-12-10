4f200f6e32717eec5aa65aee64639bd018fc897a
whdauddbs <32832910+whdauddbs@users.noreply.github.com>
2019-12-10 22:09:45 +0900

    Update MainActivity.java
    앱 종료시 MediaPlayer 객체 제거

--- app/src/main/java/wsdfhjxc/taponium/MainActivity.java
+++ app/src/main/java/wsdfhjxc/taponium/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         engineRunner.stop();
+        mediaPlayer.release();
         super.onDestroy();
     }
 
